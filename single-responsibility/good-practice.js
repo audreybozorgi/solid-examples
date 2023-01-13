a288c064ca6c9c6e87568172768aaf45160138b1
audrey <mbozorgi604@gmail.com>
2023-01-13 19:15:15 +0330

    good practice added but not finished

---
+++ single-responsibility/good-practice.js
@@ -0,0 +1,29 @@
+
+class Book {
+    constructor(name, authorName, year) {
+        this.name = name
+        this.authorName = authorName
+        this.year = year
+    }
+}
+
+class Invoice extends Book{
+    
+    constructor(name, authorName, year, price, quantity) {
+        super(name, authorName, year)
+        this.price = price
+        this.quantity = quantity
+        this.total = this.totalCalculator()
+    }
+
+    totalCalculator() {
+        let tempTotal = 0
+        tempTotal = this.price * this.quantity
+        
+        return `you are buying ${this.name} with quantity of ${this.quantity} which const you ${tempTotal}`
+    }
+}
+
+
+const myInvoice = new Invoice('Harry', 'someone', 1999, 20, 10)
+console.log(myInvoice);
